65ac71e8e809846a33de9742f4d978d2edcaa556
haduynhat2002 <nhathdth2009032@gmail.com>
2021-06-07 00:53:23 +0700

    bai tap duong pho

---
+++ QuanLiDuongPho/Program.cs
@@ -0,0 +1,15 @@
+﻿using MySql.Data.MySqlClient;
+using QuanLiDuongPho.helper;
+using QuanLiDuongPho.view;
+
+namespace QuanLiDuongPho
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            DuongPhoMenu duongPhoMenu = new DuongPhoMenu();
+            duongPhoMenu.Menu();
+        }
+    }
+}
---
+++ QuanLiDuongPho/helper/ConnectionHelper.cs
@@ -0,0 +1,33 @@
+﻿using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace QuanLiDuongPho.helper
+{
+    public class ConnectionHelper
+    {
+        private static string server = "localhost";
+        private static string database = "duongphos";
+        private static string uid = "root";
+        private static string password = "";
+        private static MySqlConnection connection;
+
+        public static MySqlConnection GetConnection()
+        {
+            if (connection == null || connection.State == ConnectionState.Closed)
+            {
+                string connectionString;
+                connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+                                   database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";" +
+                                   "convert zero datetime=True" + ";";
+
+                connection = new MySqlConnection(connectionString);
+                connection.Open();
+                return connection;
+            }
+            else
+            {
+                return connection;
+            }
+        }
+    }
+}
